b0c687ce230a9048c0dab7dcddb34d17f0778168
Azheem Rahman <azheem.rahman@gmail.com>
2022-09-13 14:02:39 +0800

    added code to hide/reveal question circles

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// to generate a random colour
+// function generates a random colour from amongst:
+// red, green, blue, yellow, white, black
 function randomColor() {
-  // red, green, blue, yellow, white, black
   let arrColors = [
     "#ff0000",
     "#008000",
@@ -27,9 +27,8 @@ let arrCircles = [
   "ten",
 ];
 
-// to generate question row of coloured circles
+// function generates question circles, each given a random colour
 function circleColor() {
-  // to put in random color for question circles
   for (let item of arrCircles) {
     let indivQCircleColour = document.querySelector(`#${item}Q`);
     indivQCircleColour.style.backgroundColor = randomColor();
@@ -38,82 +37,131 @@ function circleColor() {
 
 circleColor();
 
+// array to track which answer circles has been filled with user's input
 let userInput = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-// event listeners on the colour options to take in user's choice
-document.querySelector("#oneOption").addEventListener("click", userAnswer);
-document.querySelector("#twoOption").addEventListener("click", userAnswer);
-document.querySelector("#threeOption").addEventListener("click", userAnswer);
-document.querySelector("#fourOption").addEventListener("click", userAnswer);
-document.querySelector("#fiveOption").addEventListener("click", userAnswer);
-document.querySelector("#sixOption").addEventListener("click", userAnswer);
-
-// function adds in user's input and updates that changes have been made so that next user's click updates the next input
-function userAnswer(e) {
-  let currentChoice = userInput.indexOf(0);
-
-  if (currentChoice !== -1) {
-    switch (e.target.id) {
-      case "oneOption":
-        document.querySelector(
-          `#${arrCircles[currentChoice]}A`
-        ).style.backgroundColor = "#ff0000";
-        userInput[currentChoice] = 1;
-        break;
-      case "twoOption":
-        document.querySelector(
-          `#${arrCircles[currentChoice]}A`
-        ).style.backgroundColor = "#008000";
-        userInput[currentChoice] = 1;
-        break;
-      case "threeOption":
-        document.querySelector(
-          `#${arrCircles[currentChoice]}A`
-        ).style.backgroundColor = "#0000FF";
-        userInput[currentChoice] = 1;
-        break;
-      case "fourOption":
-        document.querySelector(
-          `#${arrCircles[currentChoice]}A`
-        ).style.backgroundColor = "#FFFF00";
-        userInput[currentChoice] = 1;
-        break;
-      case "fiveOption":
-        document.querySelector(
-          `#${arrCircles[currentChoice]}A`
-        ).style.backgroundColor = "#FFFFFF";
-        userInput[currentChoice] = 1;
-        break;
-      case "sixOption":
-        document.querySelector(
-          `#${arrCircles[currentChoice]}A`
-        ).style.backgroundColor = "#000000";
-        userInput[currentChoice] = 1;
-        break;
-    }
-  }
-}
-
-// countdown timer
+// countdown timer initally set to 10s
 let time = 10;
 
+// variable to refer to countdown timer
 const countdownElement = document.querySelector("#countdown");
 
-// user click on start button to start countdown timer
-document.querySelector("#startButton").addEventListener("click", function () {
-  let refreshIntervalId = setInterval(updateCountdown, 1000);
+// question circles initally hidden
+let questionCircles = document.querySelector("#question");
+questionCircles.style.visibility = "hidden";
 
-  function updateCountdown() {
+////////////////// user clicks on reveal button ///////////////////
+document.querySelector("#startButton").addEventListener("click", revealCircles);
+
+// revealCircles function runs when user clicks 'REVEAL!'
+function revealCircles() {
+  // reveal countdown timer starts
+  let revealInterval = setInterval(revealCountdown, 1000);
+  // question circles becomes visible for 10s
+  questionCircles.style.visibility = "visible";
+
+  function revealCountdown() {
     countdownElement.innerHTML = `${time}`;
     time--;
 
-    // to stop countdown timer once it hits 0
+    // when reveal countdown timer hits 0
     if (time < 0) {
-      clearInterval(refreshIntervalId);
-    }
+      // to stop countdown timer once it hits 0
+      clearInterval(revealInterval);
 
-    // after timer hits 0, user cannot click on anything anymore, calculate total score
+      // question circles becomes hidden
+      questionCircles.style.visibility = "hidden";
+
+      // button text change from "REVEAL!" to "START!"
+      document.querySelector("#startButton").innerText = "START!";
+    }
+  }
+  // remove event listener from button to prepare for next event listener
+  document
+    .querySelector("#startButton")
+    .removeEventListener("click", revealCircles);
+
+  // add event listener to button for user to start game
+  document.querySelector("#startButton").addEventListener("click", startGame);
+}
+
+// startGame function runs when user clicks 'START!'
+function startGame() {
+  // reset countdown timer to 15s
+  time = 15;
+  // game countdown timer starts
+  let gameInterval = setInterval(gameCountdown, 1000);
+
+  // user can start clicking on options
+  // event listeners added on option circles to take in user's choice
+  document.querySelector("#oneOption").addEventListener("click", userAnswer);
+  document.querySelector("#twoOption").addEventListener("click", userAnswer);
+  document.querySelector("#threeOption").addEventListener("click", userAnswer);
+  document.querySelector("#fourOption").addEventListener("click", userAnswer);
+  document.querySelector("#fiveOption").addEventListener("click", userAnswer);
+  document.querySelector("#sixOption").addEventListener("click", userAnswer);
+
+  // when timer starts:
+  // function adds in user's input
+  // function updates tracking array so that next user's click updates the next answer circle
+  function userAnswer(e) {
+    let currentChoice = userInput.indexOf(0);
+
+    if (currentChoice !== -1) {
+      switch (e.target.id) {
+        case "oneOption":
+          document.querySelector(
+            `#${arrCircles[currentChoice]}A`
+          ).style.backgroundColor = "#ff0000";
+          userInput[currentChoice] = 1;
+          break;
+        case "twoOption":
+          document.querySelector(
+            `#${arrCircles[currentChoice]}A`
+          ).style.backgroundColor = "#008000";
+          userInput[currentChoice] = 1;
+          break;
+        case "threeOption":
+          document.querySelector(
+            `#${arrCircles[currentChoice]}A`
+          ).style.backgroundColor = "#0000FF";
+          userInput[currentChoice] = 1;
+          break;
+        case "fourOption":
+          document.querySelector(
+            `#${arrCircles[currentChoice]}A`
+          ).style.backgroundColor = "#FFFF00";
+          userInput[currentChoice] = 1;
+          break;
+        case "fiveOption":
+          document.querySelector(
+            `#${arrCircles[currentChoice]}A`
+          ).style.backgroundColor = "#FFFFFF";
+          userInput[currentChoice] = 1;
+          break;
+        case "sixOption":
+          document.querySelector(
+            `#${arrCircles[currentChoice]}A`
+          ).style.backgroundColor = "#000000";
+          userInput[currentChoice] = 1;
+          break;
+      }
+    }
+  }
+
+  function gameCountdown() {
+    countdownElement.innerHTML = `${time}`;
+    time--;
+
+    // when countdown timer hits 0:
     if (time < 0) {
+      // stop countdown timer
+      clearInterval(gameInterval);
+
+      // question circles becomes visible
+      questionCircles.style.visibility = "visible";
+
+      // after timer hits 0, user cannot click on anything anymore
       document
         .querySelector("#oneOption")
         .removeEventListener("click", userAnswer);
@@ -133,9 +181,13 @@ document.querySelector("#startButton").addEventListener("click", function () {
         .querySelector("#sixOption")
         .removeEventListener("click", userAnswer);
       countScore();
+
+      document
+        .querySelector("#startButton")
+        .removeEventListener("click", startGame);
     }
 
-    // match user's answers to the given question and calculate total score
+    // match user's answers to the given question, calculate, and display total score
     function countScore() {
       let score = 0;
 
@@ -153,4 +205,4 @@ document.querySelector("#startButton").addEventListener("click", function () {
       document.querySelector("#score").append(printScore);
     }
   }
-});
+}
